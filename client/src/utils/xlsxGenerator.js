3bb25b3dc4bf292a4d9462e785781bc1afa13123
eduardboghian <ediboghian@yahoo.com>
2020-06-09 23:50:15 +0300

    da

--- client/src/utils/xlsxGenerator.js
+++ client/src/utils/xlsxGenerator.js
@@ -35,7 +35,7 @@ const generateXlsx = async (list, type, weekEnding) => {
   wb.Sheets['Weekly statement'] = ws;
   let editSheet = wb.Sheets['Weekly statement']
   editSheet['A1'] = { t: 's', v: `${title(weekEnding)}` }
-  editSheet['F4'] = { s: { numFmt: '#.##0,00' } }
+  editSheet['G3'] = { v: 42145.822, s: { numFmt: "#.###,00" } }
 
   let wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
 
