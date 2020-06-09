6e8e14df705c367ee5e4b4fdfff7c609c1de70d3
Robo <robo@192-168-0-100.rdsnet.ro>
2020-06-09 22:23:43 +0300

    format

--- client/src/utils/floatFormatting.js
+++ client/src/utils/floatFormatting.js
@@ -12,52 +12,58 @@ export const floatFormat = (number, type) => {
     string1 = string1 + nr[i]
   }
 
+  let j = 0
+  let string2 = ''
   if (count === 0 && type === 'hours') {
-    string1 += ',0'
-    return string1
+    string2 += '0,'
   }
   else if (count === 0) {
-    string1 += ',00'
-    return string1
+    string2 += '00,'
+  }
+  else if (string1[string1.length-3] === ',') {
+    string2 += string1[string1.length-1] + string1[string1.length-2] + string1[string1.length-3]
+  }
+  else if (string1[string1.length-2] === ',' && type === 'hours') {
+    string2 += string1[string1.length-1] + string1[string1.length-2]
   }
   else {
-    let j = 0
-    let string2 = ''
-    if (string1[string1.length-3] === ',') {
-      string2 += string1[string1.length-1] + string1[string1.length-2] + string1[string1.length-3]
-    }
-    else if (string1[string1.length-2] === ',' && type === 'hours') {
-      string2 += string1[string1.length-1] + string1[string1.length-2]
-    }
-    else {
-      string2 += '0' + string1[string1.length-1] + string1[string1.length-2]
-    }
-
-    if (string1[string1.length-3] === ',') {
-      for (let i=string1.length-4; i>=0; i--) {
-        if (j===3) {
-          string2 += '.'
-          j = 0
-        }
-        j++
-        string2 += string1[i]
-      }
-    }
-    else {
-      for (let i=string1.length-3; i>=0; i--) {
-        if (j===3) {
-          string2 += '.'
-          j = 0
-        }
-        j++
-        string2 += string1[i]
-      }
-    }
-
-    let string3 = ''
-    for (let i=string2.length-1; i>=0; i--) {
-      string3 += string2[i]
-    }
-    return string3
+    string2 += '0' + string1[string1.length-1] + string1[string1.length-2]
   }
+
+  if (string1[string1.length-3] === ',') {
+    for (let i=string1.length-4; i>=0; i--) {
+      if (j===3) {
+        string2 += '.'
+        j = 0
+      }
+      j++
+      string2 += string1[i]
+    }
+  }
+  else if (count === 0) {
+    for (let i=string1.length-1; i>=0; i--) {
+      if (j===3) {
+        string2 += '.'
+        j = 0
+      }
+      j++
+      string2 += string1[i]
+    }
+  }
+  else {
+    for (let i=string1.length-3; i>=0; i--) {
+      if (j===3) {
+        string2 += '.'
+        j = 0
+      }
+      j++
+      string2 += string1[i]
+    }
+  }
+
+  let string3 = ''
+  for (let i=string2.length-1; i>=0; i--) {
+    string3 += string2[i]
+  }
+  return string3
 }
