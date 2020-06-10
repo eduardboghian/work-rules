4dc4492a1f21ad26c1c868af47dda9616ca3a9aa
eduardboghian <ediboghian@yahoo.com>
2020-06-10 12:57:35 +0300

    true

--- client/src/utils/floatFormatting.js
+++ client/src/utils/floatFormatting.js
@@ -2,13 +2,15 @@
 export const floatFormat = (number, type) => {
   if (type === 'hours') {
     let nr = new Intl.NumberFormat('de-DE', {
-      minimumFractionDigits: 1
+      minimumFractionDigits: 1,
+      maximumFractionDigits: 1
     }).format(number)
     return nr
   }
 
   let nr = new Intl.NumberFormat('de-DE', {
-    minimumFractionDigits: 2
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
   }).format(number)
   return nr
 }
