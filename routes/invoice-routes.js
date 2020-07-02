24512c0b42cbc05af17bfc765d4b41bb82f8ff68
eduardboghian <ediboghian@yahoo.com>
2020-07-02 17:22:36 +0300

    fix bug

--- routes/invoice-routes.js
+++ routes/invoice-routes.js
@@ -25,6 +25,8 @@ router.post('/generate-invoice', async (req, res) => {
 
     site.workers.map((item, i) => {
         let ot = isNaN(parseFloat(item.rates.otRate) * parseFloat(item.worker.hoursOT)) ? 0 : parseFloat(item.rates.otRate) * parseFloat(item.worker.hoursOT)
+        item.rates.rateGot = item.rates.rateGot.replace('\,', '.')
+        item.worker.hours = item.worker.hours.replace('\,', '.')
         let rateGot = isNaN(parseFloat(item.rates.rateGot)) ? 0 : parseFloat(item.rates.rateGot).toFixed(1)
         let hours = isNaN(parseFloat(item.worker.hours)) ? '0.0' : parseFloat(item.worker.hours).toFixed(1)
 
